a61851fceed8717109cea4806e6f75d9067b290e
DangXiaofei <dxf536@163.com>
2017-06-02 16:47:14 +0800

    update doc

--- redpacketlibrary/src/com/yunzhanghu/redpacket/provider/RedPacketMsgProvider.java
+++ redpacketlibrary/src/com/yunzhanghu/redpacket/provider/RedPacketMsgProvider.java
@@ -199,7 +199,7 @@ public class RedPacketMsgProvider extends IContainerItemProvider.MessageProvider
 
             @Override
             public void hideLoading() {
-                progressDialog.hide();
+                progressDialog.dismiss();
             }
 
             @Override
